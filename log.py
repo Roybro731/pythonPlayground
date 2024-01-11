6a1d0a77e8d6f25d2698a31af8746932b9326b13
roybro <roybro731@gmail.com>
2024-01-11 13:48:36 +0200

    add logging

--- log.py
+++ log.py
@@ -1,4 +1,10 @@
+import logging
 
+def startHere():
+    logging.basicConfig(filename="log.log", level=logging.DEBUG)
+    logging.info("using logging")
+    logging.warning("this is warning")
+        
 def write(*args):
     for arg in args:
         print(arg)
